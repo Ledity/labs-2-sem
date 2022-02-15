fc4f8db2485dcea57fc68220a0aafccad9007301
ledity <voeiko.aa@phystech.edu>
2022-02-15 21:48:33 +0300

    Adding files for 2.1.1. and 2.1.5.

---
+++ 2.1.1./.auctex-auto/2.1.1.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "2.1.1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1" "T2A") ("inputenc" "utf8") ("babel" "english" "russian")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "graphicx"
+    "fontenc"
+    "inputenc"
+    "babel")
+   (LaTeX-add-labels
+    "eq1"))
+ :latex)
+
