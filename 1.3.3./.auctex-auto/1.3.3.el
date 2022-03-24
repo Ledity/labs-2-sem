2c67358820646a8c2e43e7298944576bb5cadcb4
ledity <voeiko.aa@phystech.edu>
2022-03-24 22:36:40 +0300

    sas sos sus ses

---
+++ 1.3.3./.auctex-auto/1.3.3.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "1.3.3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("mathtext" "warn") ("fontenc" "T1" "T2A") ("inputenc" "utf8") ("babel" "english" "russian")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "graphicx"
+    "mathtext"
+    "fontenc"
+    "inputenc"
+    "babel"
+    "pgfplots"))
+ :latex)
+
